a17f3770e9613876d220f5aabdd7b31d0603ba84
Skeblos <s.becak@seznam.cz>
2025-07-13 10:36:14 +0200

    backport to functional page

--- app.js
+++ app.js
@@ -1,17 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import reportError from './reportError';
-
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import Services from './components/Services';
-import Process from './components/Process';
-import Tools from './components/Tools';
-import Projects from './components/Projects';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
-
 function App() {
     try {
         return (
